746c6c3499f8bb89637fe0d6537d46715d55c4b4
Shola <118860644+Sholly45@users.noreply.github.com>
2025-02-16 00:00:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
-# main.tf - Terraform configuration file for S3 bucket
-
+# Define the AWS provider
 provider "aws" {
-  region = "eu-west-2"  # Change to your preferred AWS region
+  region = "eu-west-2"  # Change to your AWS region
 }
 
+# Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # Use a globally unique name
-  acl    = "private"
+  bucket = "my-unique-bucket-name-12345"  # Use a unique bucket name
 
   tags = {
-    Name        = "MyS3Bucket"
+    Name        = "MyBucket"
     Environment = "Dev"
   }
+}
+
+# Set ACL for the bucket
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
